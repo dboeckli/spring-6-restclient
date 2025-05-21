fb86c63a3a1eab5e783e2affa4e4776d5b76cc38
adm-boeckli <dboeckli@gmail.com>
2025-05-21 13:57:00 +0200

    remove unnecessary comments

--- src/main/java/guru/springframework/spring6restclient/dto/BeerStyle.java
+++ src/main/java/guru/springframework/spring6restclient/dto/BeerStyle.java
@@ -1,8 +1,5 @@
 package guru.springframework.spring6restclient.dto;
 
-/**
- * Created by jt, Spring Framework Guru.
- */
 public enum BeerStyle {
     LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }

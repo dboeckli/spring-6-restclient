fd1a68ebdf70cf91ddc1725481522707a929deca
Dominique Boeckli <dboeckli@gmail.com>
2026-08-23 12:45:58 +0200

    add helm test

---
+++ .run/scripts/test-k8s.ps1
@@ -0,0 +1,6 @@
+cd target/helm/repo
+
+$file = Get-ChildItem -Filter spring-6-restclient-chart-*.tgz | Select-Object -First 1
+$APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
+Write-Host "test application: $APPLICATION_NAME"
+helm test $APPLICATION_NAME --namespace spring-6-restclient --logs
